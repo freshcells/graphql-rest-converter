b7b3d24532df38668f0d99881d60ce5b61042475
David Heidrich <me@bowlingx.com>
2024-03-28 16:20:39 +0100

    fix: allow passing a custom scalar base class

--- src/graphql-upload.ts
+++ src/graphql-upload.ts
@@ -1,4 +1,9 @@
-export { GraphQLUploads, GraphQLUpload } from './graphql-upload/scalars.js'
+export {
+  GraphQLUploads,
+  GraphQLUpload,
+  createGraphQLUploads,
+  createGraphQLUpload,
+} from './graphql-upload/scalars.js'
 export { processRequest, FileUpload } from './graphql-upload/processRequest.js'
 
 export { UploadScalars } from './graphql-upload/openapi.js'
