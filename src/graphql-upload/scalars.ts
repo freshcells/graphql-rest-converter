71bb8357ff71cdb0e5ed0f133d86d16af8cbbc08
David Heidrich <me@bowlingx.com>
2024-03-28 16:20:39 +0100

    fix: allow passing a custom scalar base class

--- src/graphql-upload/scalars.ts
+++ src/graphql-upload/scalars.ts
@@ -1,25 +1,17 @@
 // credits: https://github.com/jaydenseric/graphql-upload/blob/master/GraphQLUpload.mjs
 
-import { GraphQLError, GraphQLScalarType } from 'graphql'
+import { GraphQLError, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql'
 
 import Upload from './Upload.js'
 import { AsyncQueue } from '../iterable.js'
+import { Class } from 'type-fest'
 
-/**
- * A GraphQL `Upload` scalar that can be used in a
- * [`GraphQLSchema`](https://graphql.org/graphql-js/type/#graphqlschema). It’s
- * value in resolvers is a promise that resolves
- * {@link FileUpload file upload details} for processing and storage.
- */
-export const GraphQLUpload = new GraphQLScalarType({
+const GraphQLUploadConfig: GraphQLScalarTypeConfig<unknown, unknown> = {
   name: 'Upload',
   description: 'The `Upload` scalar type represents a single file upload.',
   parseValue(value) {
     if (value instanceof Upload) {
       return value.promise
-    }
-    if (value instanceof AsyncQueue) {
-      return value
     }
     throw new GraphQLError('Upload value invalid.')
   },
@@ -29,15 +21,20 @@ export const GraphQLUpload = new GraphQLScalarType({
   serialize() {
     throw new GraphQLError('Upload serialization unsupported.')
   },
-})
+}
 
 /**
- * A GraphQL `Uploads` scalar that can be used in a
+ * A GraphQL `Upload` scalar that can be used in a
  * [`GraphQLSchema`](https://graphql.org/graphql-js/type/#graphqlschema). It’s
- * value in resolvers is a AsyncGenerator that generates
+ * value in resolvers is a promise that resolves
  * {@link FileUpload file upload details} for processing and storage.
  */
-export const GraphQLUploads = new GraphQLScalarType({
+export const GraphQLUpload = new GraphQLScalarType(GraphQLUploadConfig)
+
+export const createGraphQLUpload = (type: Class<unknown>) =>
+  new type(GraphQLUploadConfig) as GraphQLScalarType
+
+const GraphQLUploadsConfig: GraphQLScalarTypeConfig<unknown, unknown> = {
   name: 'Uploads',
   description: 'The `Uploads` scalar type represents multiple file uploads.',
   parseValue(value) {
@@ -52,4 +49,15 @@ export const GraphQLUploads = new GraphQLScalarType({
   serialize() {
     throw new GraphQLError('Uploads serialization unsupported.')
   },
-})
+}
+
+/**
+ * A GraphQL `Uploads` scalar that can be used in a
+ * [`GraphQLSchema`](https://graphql.org/graphql-js/type/#graphqlschema). It’s
+ * value in resolvers is a AsyncGenerator that generates
+ * {@link FileUpload file upload details} for processing and storage.
+ */
+export const GraphQLUploads = new GraphQLScalarType(GraphQLUploadsConfig)
+
+export const createGraphQLUploads = (type: Class<unknown>) =>
+  new type(GraphQLUploadsConfig) as GraphQLScalarType
